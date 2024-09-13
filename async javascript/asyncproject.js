e086da6d23ae21cc6a18b71c708042566f704b13
vishal-pillai-dev <ignvishal@gmail.com>
2024-09-14 03:27:05 +0530

    updated

---
+++ async javascript/asyncproject.js
@@ -0,0 +1,56 @@
+//promises
+// 
+function getdata1(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(() => {
+            console.log("getting some data.....")
+          let error= false;
+          if( error==true){
+
+            reject("SOME ERROR OCCURED")
+          }else {
+            resolve({username:"ananaya",age:36,gender:"lesbian"})
+          }
+        }, 4000);
+    })
+}
+async function gettingdata1(){
+    try {
+        let response= await getdata1();
+        console.log(response)
+        setTimeout(()=>{
+            console.log("DATA SUCCESSFULLY FETCHED !!..")
+    
+        },2000)
+        
+    } catch (error) {
+        console.log("E: some error occured")
+    }
+}
+
+gettingdata1();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
