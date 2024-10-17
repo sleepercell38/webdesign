3e335c2ad32aa3b7ae6603b7d6409bdbc755b92b
vishal-pillai-dev <ignvishal@gmail.com>
2024-10-18 01:57:26 +0530

    updated

---
+++ react_main_2/src/components/Lists.jsx
@@ -0,0 +1,53 @@
+// import React from 'react'
+
+// const Lists = () => {
+
+
+//     const fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew"];
+
+//     const fruititems= fruits.map((elems,index)=>{
+//             return <li key={index}> {elems}</li>
+//     })
+ 
+//   return (
+//    <ul>{fruititems}</ul>
+//   )
+// }
+
+// export default Lists
+
+
+
+// rendering list with array of objects 
+
+
+
+
+// import React, { useLayoutEffect } from 'react'
+
+// const Lists = () => {
+
+
+//     const fruits = [
+//         { name: "apple", calories: 52 },
+//         { name: "banana", calories: 96 },
+//         { name: "cherry", calories: 50 },
+//         { name: "date", calories: 282 },
+//         { name: "elderberry", calories: 73 },
+//         { name: "fig", calories: 74 },
+//         { name: "grape", calories: 69 },
+//         { name: "honeydew", calories: 36 }
+//       ];
+
+//       const fruititems= fruits.map((elems,calories)=>{
+//         return <div><li className='flex justify-evenly ' key={calories}> FRUIT - {elems.name}  <li>CALORIES - {elems.calories}</li></li></div>
+//       })
+      
+//   return (<ul>{fruititems}</ul>)
+// }
+
+// export default Lists
+
+
+
+// sorting with array of objects 
---
+++ react_main_2/src/list_rendering.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Lists from './components/Lists'
+
+const list_rendering = () => {
+  return (
+  <Lists/>
+  )
+}
+
+export default list_rendering
